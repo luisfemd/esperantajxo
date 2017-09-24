1f477996372010ce4b2fa8e0b9dcc7f1f63961fc
Luis Felipe Montoya <luisfemd@gmail.com>
2017-09-23 21:02:42 -0500

    module skribsistemo added

--- lib/diakrito/skribsistemo.ex
+++ lib/diakrito/skribsistemo.ex
@@ -1,14 +1,16 @@
+defmodule Esperantajxo.Diakrito.Skribsistemo do
 
-
-@diacrits %{cx: ~s(\u{108}), gx: ~s(\u{11d}), hx: ~s(\u{124}), jx: ~s(\u{135}), sx: ~s(\u{15c}), ux: ~s(\u{16d}),
+  @diacrits %{cx: ~s(\u{108}), gx: ~s(\u{11d}), hx: ~s(\u{124}), jx: ~s(\u{135}), sx: ~s(\u{15c}), ux: ~s(\u{16d}),
           C: ~s(\u{109}), G: ~s(\u{11e}), H: ~s(\u{125}), J: ~s(\u{136}), S: ~s(\u{15d}), U: ~s(\u{16c})}
 
 
-def from_x_system(text) do
+  def from_x_system(text) do
 
-end
+  end
 
 
-def from_h_system(text) do
+  def from_h_system(text) do
+
+  end
 
 end
